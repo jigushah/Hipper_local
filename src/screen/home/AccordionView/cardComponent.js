09918cbf8c6c0fe41787e1fe74cf055260b60be9
Vivek Choksi <mail.vivekchoksi@gmail.com>
2017-09-24 15:55:39 +0530

    Adds scan image and close button for scan view

--- src/screen/home/AccordionView/cardComponent.js
+++ src/screen/home/AccordionView/cardComponent.js
@@ -7,20 +7,22 @@ const listImg = require('../../../../assets/images/calzone.jpg');
 const callImg = require('../../../../assets/images/calling.png')
 const scanImg = require('../../../../assets/images/scan_me_btn.png')
 const mapImg = require('../../../../assets/images/maps_logo.png')
+const qrImg = require('../../../../assets/images/qr_code.png')
 const closeImg = require('../../../../assets/images/cross_BW.png')
 
 export default class cardComponent extends Component {
   constructor(){
     super();
     this.state = {
-      mapView:false
+      mapView:false,
+      scanView:false
     }
   }
-
   onScanClick = () =>{
-    alert("Scan clicked");
+    this.setState({
+      scanView:true
+    })
   }
-
   onMapClick = () => {
     this.setState({
       mapView:true
@@ -29,8 +31,16 @@ export default class cardComponent extends Component {
 
   onMapCloseClick = () => {
     this.setState({
-      mapView:false
+      mapView:false,
+      scanView:false
     })
+  }
+  onScanCloseClick = () => {
+    this.setState(
+      {
+        mapView:false,
+        scanView:false
+      })
   }
 
   render() {
@@ -41,20 +51,29 @@ export default class cardComponent extends Component {
         </Text>
 
         <View style={{height:200}}>
-        {(this.state.mapView)?
+          {(this.state.mapView)?
           <View style={{height:200}}>
             <TouchableHighlight style={{height:30, width:'30%', alignSelf:'flex-end'}} onPress={() => this.onMapCloseClick()}>
               <Image resizeMode={'contain'} style={{height:30, width:'100%'}} source={closeImg}/>
             </TouchableHighlight>
             <Map/>
           </View>
-          :<Image style={{height:200,width:300,marginRight:10,marginLeft:10,alignSelf:'center' }} source={listImg}/>}
+          :
+          (this.state.scanView)?
+            <View>
+            <TouchableHighlight style={{height:30, width:'30%', alignSelf:'flex-end'}} onPress={() => this.onScanCloseClick()}>
+              <Image resizeMode={'contain'} style={{height:30, width:'100%'}} source={closeImg}/>
+            </TouchableHighlight>
+            <Image style={{height:200, width:300,marginRight:10,marginLeft:10,alignSelf:'center'}} source={qrImg}/>
+            </View>
+            :<Image style={{height:200, width:300,marginRight:10,marginLeft:10,alignSelf:'center'}} source={listImg}/>}
         </View>
+
         <View style={{marginTop:20, flexDirection:'row',justifyContent:'space-between'}}>
-        <TouchableHighlight style={{height:50, width:'30%', alignSelf:'flex-end'}} onPress={() => this.onScanClick()}>
-          <Image resizeMode={'contain'} style={{height:50, width:'100%', alignSelf:'flex-start'}} source={scanImg}/>
-        </TouchableHighlight>
-          <Image resizeMode={'contain'} style={{height:50, width:'30%', alignSelf:'center'}} source={callImg}/>
+          <TouchableHighlight style={{height:50, width:'30%', alignSelf:'flex-end'}} onPress={() => this.onScanClick()}>
+            <Image resizeMode={'contain'} style={{height:50, width:'100%', alignSelf:'flex-start'}} source={scanImg}/>
+          </TouchableHighlight>
+            <Image resizeMode={'contain'} style={{height:50, width:'30%', alignSelf:'center'}} source={callImg}/>
           <TouchableHighlight style={{height:50, width:'30%', alignSelf:'flex-end'}} onPress={() => this.onMapClick()}>
             <Image resizeMode={'contain'} style={{height:50, width:'100%'}} source={mapImg}/>
           </TouchableHighlight>
